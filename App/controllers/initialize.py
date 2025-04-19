0f49f7a853ae0e6290c0f584ed8715a26cd32f9c
Aioblitz <60820112+Aioblitz@users.noreply.github.com>
2025-04-19 03:17:35 -0400

    Filter Faculty

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -6,7 +6,7 @@ def initialize():
     db.drop_all()
     db.create_all()
     create_user('bob', 'bobpass')
-    create_marker('uwi1',10.6398,-61.3997,'Engineering',1)
-    create_marker('uwi2',10.6394,-61.3993,'Engineering',1)
-    create_marker('This is a long name',10.6390,-61.3990,'Engineering',1)
+    create_marker('Eng',10.6398,-61.3997,'Engineering',1)
+    create_marker('FST',10.6394,-61.3993,'Science and Technology',1)
+    create_marker('Law',10.6390,-61.3990,'Law',1)
 
--- App/views/user.py
+++ App/views/user.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, jsonify, request, send_from_direct
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 
 from.index import index_views
+from App.controllers.marker import *
 
 from App.controllers import (
     create_user,
@@ -42,4 +43,5 @@ def static_user_page():
 @user_views.route('/editmap', methods=['GET'])
 @jwt_required()
 def editmap_page():
-    return render_template('editmap.html')
+    markers = get_all_markers_json()
+    return render_template('editmap.html',markers=markers)
